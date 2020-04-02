6d6679549bd31e74acd3e25e3aa82ca7e3ad366a
cgonz1286 <60245968+cgonz1286@users.noreply.github.com>
2020-04-01 22:01:37 -0500

    Implementing Spring MVC functionality

--- src/main/java/dmacc/controller/BeanConfiguration.java
+++ src/main/java/dmacc/controller/BeanConfiguration.java
@@ -1,20 +1,20 @@
-package dmacc.controller;
-
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-
-import dmacc.beans.Home;
-
-@Configuration
-public class BeanConfiguration {
-
-	@Bean
-	@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-	public Home home() {
-		Home bean = new Home();
-		return bean;
-	}
-	
-}
+//package dmacc.controller;
+//
+//import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Scope;
+//
+//import dmacc.beans.Home;
+//
+//@Configuration
+//public class BeanConfiguration {
+//
+//	@Bean
+//	@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//	public Home home() {
+//		Home bean = new Home();
+//		return bean;
+//	}
+//	
+//}
